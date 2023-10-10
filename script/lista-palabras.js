824df268a1fa96f38a9131bbadceaf148dd168db
unknown <juanchdzk_12@hotmail.com>
2023-10-10 15:40:57 -0300

    Primera version del desafio de Alura Latam - Ahorcado

---
+++ script/lista-palabras.js
@@ -0,0 +1,66 @@
+const listaPalabras = [
+    {
+        palabra: "computadora",
+        pista: "Máquina que obedece órdenes para efectuar cálculos y operaciones lógicas en poco tiempo."
+    },
+    {
+        palabra: "ecosistema",
+        pista: "Conjunto de especies de un área determinada que interactúan entre ellas y con su ambiente abiótico."
+    },
+    {
+        palabra: "programacion",
+        pista: "Organizar una secuencia de pasos ordenados a seguir para hacer cierta cosa."
+    },
+    {
+        palabra: "meteorito",
+        pista: "Objeto que ha caído a la Tierra desde el espacio."
+    },
+    {
+        palabra: "biblioteca",
+        pista: "Institución cuya finalidad consiste en la adquisición, conservación, estudio y exposición de libros y documentos."
+    },
+    {
+        palabra: "guitarra",
+        pista: " Instrumento musical de la familia de los cordófonos, es decir los instrumentos que producen su sonido al hacer vibrar las cuerdas."
+    },
+    {
+        palabra: "estomago",
+        pista: " Porción del sistema digestivo que se ocupa de descomponer los alimentos."
+    },
+    {
+        palabra: "tornado",
+        pista: "Es uno de los fenómenos más severos y destructivos que se producen sobre la superficie de la tierra."
+    },
+    {
+        palabra: "verdura",
+        pista: "Hortaliza que se cultiva especialmente para aprovechar las hojas y los tallos tiernos y las inflorescencias, y que se come fresca en ensalada o también cocida."
+    },
+    {
+        palabra: "electricidad",
+        pista: "Forma de energía que se manifiesta con el movimiento de los electrones de la capa externa de los átomos que hay en la superficie de un material conductor."
+    },
+    {
+        palabra: "baldosa",
+        pista: "Una losa o loseta manufacturada, fabricada en diferentes tipos y técnicas de cerámica, así como en piedra, caucho, corcho, vidrio, metal, plástico, etc."
+    },
+    {
+        palabra: "familia",
+        pista: "Grupo de personas, normalmente unidas por lazos legales, que conviven y tienen un proyecto de vida en común."
+    },
+    {
+        palabra: "negocio",
+        pista: "Ocupación, actividad o trabajo que se realiza para obtener un beneficio, especialmente el que consiste en realizar operaciones comerciales, comprando y vendiendo mercancías o servicios."
+    },
+    {
+        palabra: "comunicacion",
+        pista: "Transmisión de señales mediante un código común al emisor y al receptor."
+    },
+    {
+        palabra: "conocimiento",
+        pista: "Facultad del ser humano para comprender por medio de la razón la naturaleza, cualidades y relaciones de las cosas."
+    },
+    {
+        palabra: "onomatopeya",
+        pista: "Palabra que tiene sonidos que se asemejan a lo que significa."
+    }
+];
---
+++ script/script.js
@@ -0,0 +1,80 @@
+const ahorcadoImagen = document.querySelector(".hangman-box img");
+const mostrarPalabra = document.querySelector(".word-display");
+const adivinaTexto = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const modeloGame = document.querySelector(".game-modal");
+const jugardeNuevoBtn = document.querySelector(".play-again");
+
+let palabraActual, resptCorrectas, contadorRespMal;
+const maxAdivinar = 6;
+
+// Reinicia todas las variables del juego y los elementos UI (user interface) 
+const reinicioGame = () => {
+    resptCorrectas = [];
+    contadorRespMal = 0;
+    ahorcadoImagen.src = `imagesHangman/hangman-${contadorRespMal}.svg`;
+    adivinaTexto.innerText = `${contadorRespMal} / ${maxAdivinar}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    mostrarPalabra.innerHTML = palabraActual.split("").map(() => `<li class="letter"></li>`).join("");
+    mostrarPalabra.style.setProperty("filter", "invert(0.95)"); 
+    modeloGame.classList.remove("show");
+}
+
+// Selecciona una palabra aleatoria y la pista de la palabra
+const getRandomWord = () => {
+    const { palabra, pista } = listaPalabras[Math.floor(Math.random() * listaPalabras.length)];
+    palabraActual = palabra;
+    // console.log(palabra);
+    document.querySelector(".hint-text b").innerHTML = pista;
+    reinicioGame();
+}
+
+// Cartel final de la partida y sus características 
+const gameOver = (isVictory) => {
+    setTimeout(() => {
+        const modalText = isVictory ? `Adivinista la palabra` : `La palabra correcta era:`;
+        modeloGame.querySelector("img").src = `imagesHangman/${isVictory ? 'victory' : 'lost'}.gif`;
+        modeloGame.querySelector("h4").innerText = `${isVictory ? '¡Felicidades!' : 'Game Over!'}`;
+        modeloGame.querySelector("p").innerHTML = `${modalText} <b>${palabraActual}</b>`;
+        modeloGame.classList.add("show");
+    }, 300);
+}
+
+// Identifica si existe la letra en la palabra 
+const initGame = (button, clickedLetter) => {
+    if(palabraActual.includes(clickedLetter)){
+        // Muestra todas las letras correctas en la visualización de palabras
+        [...palabraActual].forEach((letter, index) => {
+            if(letter === clickedLetter){
+                resptCorrectas.push(letter);
+                mostrarPalabra.querySelectorAll("li")[index].innerText = letter;
+                mostrarPalabra.querySelectorAll("li")[index].classList.add("adivinado");
+            }
+        })
+    } else {
+        // Si seleccionas una letra que no existe entonces se actualizará tanto el contador como la imagen del sujeto en la horca.
+        contadorRespMal++;
+        ahorcadoImagen.src = `imagesHangman/hangman-${contadorRespMal}.svg`;
+    }
+
+    button.disabled = true;
+    adivinaTexto.innerText = `${contadorRespMal} / ${maxAdivinar}`;
+
+    // Llamar a la funcion gameOvr si alguna de estas condiciones se cumple
+    if(contadorRespMal === maxAdivinar) return gameOver(false);
+    if(resptCorrectas.length === palabraActual.length) return gameOver(true);
+}
+
+// Crear botones de teclado y, añadir un evento del boton y la letra seleccionada
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame(e.target, String.fromCharCode(i)));
+}
+
+getRandomWord();
+jugardeNuevoBtn.addEventListener("click", getRandomWord);
+
+// init(abreviatura de initialization) es el primer preoceso en ejecución tras la carga del núcleo y el que a su vez genera todos los demás procesos.
+
